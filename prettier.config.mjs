22abd2d4f1fcdc7e714d793534ff3f995f73307e
dahoon06 <dahoon226@naver.com>
2026-05-18 01:01:52 +0900

    init

---
+++ prettier.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('prettier').Config} */
+const config = {
+    plugins: ['prettier-plugin-tailwindcss'],
+    singleQuote: true,
+    semi: true,
+    arrowParens: 'always',
+    trailingComma: 'all',
+    printWidth: 100,
+    tabWidth: 4,
+};
+
+export default config;
